b705674ece4f6ffefa5b7dca745db3ebe19e6d68
Nic Smith <thesonicsmith@gmail.com>
2023-06-11 21:06:46 +1200

    Remove @t3-oss dependency

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,3 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import('./src/env.mjs');
-
 // const withTM = require('next-transpile-modules')(['three'])
 
 /** @type {import("next").NextConfig} */
