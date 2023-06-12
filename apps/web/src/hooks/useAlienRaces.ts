169824b2fe3e17b599ec2f733860ee070feb8cf6
Nic Smith <thesonicsmith@gmail.com>
2023-06-12 20:28:42 +1200

    Revert "Temporarily test without chain dependency"
    This reverts commit 1e5284b89a57f6aad9377f35d2f6ed1b1eaa28f9.

--- apps/web/src/hooks/useAlienRaces.ts
+++ apps/web/src/hooks/useAlienRaces.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-// import alienJson from 'chain/artifacts/contracts/Aliens.sol/Aliens.json';
-// import battlefieldJson from 'chain/artifacts/contracts/BattlefieldEarth.sol/BattlefieldEarth.json';
+import alienJson from 'chain/artifacts/contracts/Aliens.sol/Aliens.json';
+import battlefieldJson from 'chain/artifacts/contracts/BattlefieldEarth.sol/BattlefieldEarth.json';
 
 import { FUNCTIONS, MUMBAI, ADDRESSES } from '~/data/contracts';
 import { readContracts, useContractRead, useNetwork } from 'wagmi';
@@ -23,40 +23,40 @@ export const useAlienRaces = () => {
   const [aliens, setAliens] = useState<AlienRace[]>([]);
   const { chain } = useNetwork();
 
-  // const { data: alienData } = useContractRead({
-  //   address: ADDRESSES[chain?.id || MUMBAI]!.BATTLEFIELD,
-  //   abi: battlefieldJson.abi,
-  //   functionName: FUNCTIONS.ALIENS.getAliens,
-  // });
+  const { data: alienData } = useContractRead({
+    address: ADDRESSES[chain?.id || MUMBAI]!.BATTLEFIELD,
+    abi: battlefieldJson.abi,
+    functionName: FUNCTIONS.ALIENS.getAliens,
+  });
 
-  // useEffect(() => {
-  //   const fetchAliens = async () => {
-  //     const contracts = (alienData as BattlefieldAliens[]).map(
-  //       ({ tokenId }) => ({
-  //         address: ADDRESSES[chain?.id || MUMBAI]!.ALIENS,
-  //         abi: alienJson.abi,
-  //         functionName: FUNCTIONS.ALIENS.getAlienStrength,
-  //         args: [tokenId],
-  //       })
-  //     );
-  //     const strengthData = (await readContracts({ contracts })) as BigInt[];
-  //     setAliens(
-  //       (alienData as BattlefieldAliens[]).map(({ tokenId }, index) => {
-  //         const stringNumber = getThreeDigitNumber(tokenId);
-  //         return {
-  //           image: `/images/aliens/${stringNumber}.jpg`,
-  //           description: alienSpecies[tokenId]?.description || '',
-  //           color: alienSpecies[tokenId]?.color || '#FFF',
-  //           power: strengthData?.[index]?.toString() || '0',
-  //         };
-  //       })
-  //     );
-  //   };
+  useEffect(() => {
+    const fetchAliens = async () => {
+      const contracts = (alienData as BattlefieldAliens[]).map(
+        ({ tokenId }) => ({
+          address: ADDRESSES[chain?.id || MUMBAI]!.ALIENS,
+          abi: alienJson.abi,
+          functionName: FUNCTIONS.ALIENS.getAlienStrength,
+          args: [tokenId],
+        })
+      );
+      const strengthData = (await readContracts({ contracts })) as BigInt[];
+      setAliens(
+        (alienData as BattlefieldAliens[]).map(({ tokenId }, index) => {
+          const stringNumber = getThreeDigitNumber(tokenId);
+          return {
+            image: `/images/aliens/${stringNumber}.jpg`,
+            description: alienSpecies[tokenId]?.description || '',
+            color: alienSpecies[tokenId]?.color || '#FFF',
+            power: strengthData?.[index]?.toString() || '0',
+          };
+        })
+      );
+    };
 
-  //   if ((alienData as BattlefieldAliens[])?.length) {
-  //     fetchAliens();
-  //   }
-  // }, [alienData]);
+    if ((alienData as BattlefieldAliens[])?.length) {
+      fetchAliens();
+    }
+  }, [alienData]);
 
   return aliens;
 };
