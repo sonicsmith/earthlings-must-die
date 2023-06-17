766e07151db5cec716e4cdf0ea66074f7b295a89
Nic Smith <thesonicsmith@gmail.com>
2023-06-17 21:24:23 +1200

    Add image metadata

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -9,8 +9,9 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Earthlings Must Die</title>
-        <meta name="description" content="" />
+        <meta name="description" content="Play to Earn Invasion Game" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="/images/aliens/001.jpg" />
       </Head>
       <main className="h-screen">
         <Scene />
