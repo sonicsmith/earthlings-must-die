402e036748c5bff5d0842a0f03e7af691dc28068
Nic Smith <thesonicsmith@gmail.com>
2023-10-27 20:41:12 +1300

    Improve Menu button

--- apps/web/src/components/GUI/ProfileButton.tsx
+++ apps/web/src/components/GUI/ProfileButton.tsx
@@ -1,4 +1,5 @@
 import { useAppStore } from '~/store/appStore';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 export const ProfileButton = () => {
   const { setIsAlienSelectionView, showMenu, setShowMenu, email } =
@@ -12,11 +13,10 @@ export const ProfileButton = () => {
         setIsAlienSelectionView(false);
         setShowMenu(!showMenu);
       }}
-      className={
-        'mx-2 rounded bg-brand px-3 py-1 text-black hover:cursor-pointer'
-      }
+      className={'mx-2 my-1 flex text-brand hover:cursor-pointer'}
     >
       {display}
+      <Bars3Icon className="my-auto ml-2 h-5 w-5" />
     </div>
   );
 };
